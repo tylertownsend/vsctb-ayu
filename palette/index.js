1bd287e5d35ec786323ccc429b7c1e15d726d07c
Tyler Townsend <townsend.ts@outlook.com>
2020-08-16 12:50:27 -0700

    initial ayu colors

---
+++ palette/index.js
@@ -0,0 +1,34 @@
+const colors = {
+  // Black and White
+  black: "#00010a",
+  white: "#ffffff",
+  
+  // Traditional Colors
+  red: "#f51818", //0
+  orange: "#fa8d3e", //1
+  yellow: "#f2ae49", //2
+  green: "#86b300", //3
+  cyan: "#4cbf99", //4
+  blue: "#399ee6", //5
+  purple: "#a37acc", //6
+  magnenta: "#f07171",//7
+
+  // Accent Colors
+  accent0: "#ff9940",
+  accent1: "#55b4d4",
+  accent2: "#4cbf99",
+  accent3: "#f2ae49",
+
+  // Background to Foreground Spectrum
+  // Sort from darkest to brightest
+  shade0: "#232834",
+  shade1: "#33415e",
+  shade2: "#586069",
+  shade3: "#6a737d",
+  shade4: "#959da5",
+  shade5: "#d1d5da",
+  shade6: "#e1e1e1",
+  shade7: "#f0f0f0"
+}
+
+module.exports = colors;
